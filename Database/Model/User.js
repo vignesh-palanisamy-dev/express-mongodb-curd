36b5ec98bdca8e36c60b8f3458629577623f8843
vignespacap <vignesh.palanisamy@capillarytech.com>
2022-05-03 16:47:52 +0530

    comments added

--- Database/Model/User.js
+++ Database/Model/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-// Making table
+// Creating table / modal
+// Here table name - User, Column names : name, age
 const User = mongoose.model("User", {
   name: { type: String },
   age: { type: Number },
